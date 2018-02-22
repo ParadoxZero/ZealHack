bc4b6bc4bd99ec6ecf6bca8b5b4543881aaaff33
hadeeb <hadeebfarhan1@gmail.com>
2018-02-23 04:54:11 +0530

    Added push message-client

--- wisdom/static/wisdom/js/sw-reg.js
+++ wisdom/static/wisdom/js/sw-reg.js
@@ -1,16 +1,72 @@
 if ('serviceWorker' in navigator && 'PushManager' in window) {
     console.log('Service Worker and Push is supported');
-  
     navigator.serviceWorker.register('sw.js')
     .then(function(swReg) {
       console.log('Service Worker is registered', swReg);
   
       swRegistration = swReg;
+      askPermission();
     })
     .catch(function(error) {
       console.error('Service Worker Error', error);
     });
-  } else {
+} else {
     console.warn('Push messaging is not supported');
     pushButton.textContent = 'Push Not Supported';
-  }
+}
+function askPermission() {
+  return new Promise(function(resolve, reject) {
+    const permissionResult = Notification.requestPermission(function(result) {
+      resolve(result);
+    });
+
+    if (permissionResult) {
+      permissionResult.then(resolve, reject);
+    }
+  })
+  .then(function(permissionResult) {
+    if (permissionResult !== 'granted') {
+      throw new Error('We weren\'t granted permission.');
+    } else if(permissionResult === 'granted') {
+      subscribeUserToPush();
+    }
+  });
+}
+function subscribeUserToPush() {
+  return navigator.serviceWorker.register('sw.js')
+  .then(function(registration) {
+    const subscribeOptions = {
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array(
+        'BJmGp1HQa4EejFwPuUgG4QVpZb2AoSjhx8HeFoWW8bcmqQyNsdCPDgcSLcoVg5jnqkk5ZNAXXxYIigMMtpf4DR0'
+      )
+    };
+
+    return registration.pushManager.subscribe(subscribeOptions);
+  })
+  .then(function(pushSubscription) {
+    console.log('Received PushSubscription: ', JSON.stringify(pushSubscription));
+    sendSubscriptionToBackEnd(pushSubscription);
+  });
+}
+function sendSubscriptionToBackEnd(subscription) {
+  return fetch('/save/subscription/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(subscription)
+  })
+  .then(function(response) {
+    if (!response.ok) {
+      throw new Error('Bad status code from server.');
+    }
+
+    return response.json();
+  })
+  .then(function(responseData) {
+    if (!(responseData.data && responseData.data.success)) {
+      throw new Error('Bad response from server.');
+    }
+  });
+}
